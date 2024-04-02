22d4fcc701b89545fca440efcdd9d42fea2e9bd5
ayozemt <ayozemt@gmail.com>
2024-04-02 14:34:00 +0200

    Create subject table

--- src/components/TeacherCard/TeacherCard.tsx
+++ src/components/TeacherCard/TeacherCard.tsx
@@ -4,16 +4,21 @@ import Teacher from "../../interfaces/Teacher";
 
 interface Props {
   teacher: Teacher;
+  onClick: () => void;
+  isSelected: boolean;
 }
 
-function TeacherCard({ teacher }: Props) {
+function TeacherCard({ teacher, onClick, isSelected }: Props) {
   return (
-    <div className="card">
+    <div className={`cardo ${isSelected ? "selected" : ""}`} onClick={onClick}>
       <img src={teacher.photo} alt="Teacher" style={{ height: "100px" }} />
       <div>
         <p>{teacher.name}</p>
         <p>{teacher.email}</p>
         <p>{teacher.telephone}</p>
+      </div>
+      <div>
+        <a href={`/editar/${teacher._id}`}>Editar</a>
       </div>
     </div>
   );
--- src/pages/TeachersPage/TeachersPage.tsx
+++ src/pages/TeachersPage/TeachersPage.tsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import TeacherCard from "../../components/TeacherCard/TeacherCard";
 import Teacher from "../../interfaces/Teacher";
+import Subject from "../../interfaces/Subject";
 import { getTeachers } from "../../services/TeacherService";
+import { getSubjectsByTeacher } from "../../services/SubjectService";
 
 function TeachersPage() {
   const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const [subjects, setSubjects] = useState<{ [key: string]: Subject[] }>({});
+  const [selectedTeacherId, setSelectedTeacherId] = useState<string | null>(
+    null
+  );
 
   useEffect(() => {
     fetchTeachers();
@@ -19,14 +25,73 @@ function TeachersPage() {
     }
   };
 
+  const handleTeacherClick = async (teacherId: string) => {
+    try {
+      if (selectedTeacherId === teacherId) {
+        setSelectedTeacherId(null);
+      } else {
+        setSelectedTeacherId(teacherId);
+        if (!subjects[teacherId]) {
+          const fetchedSubjects = await getSubjectsByTeacher(teacherId);
+          setSubjects({ ...subjects, [teacherId]: fetchedSubjects });
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching subjects:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Profesores</h1>
       <h2>Crea y gestiona profesores</h2>
 
       {teachers.map((teacher) => (
-        <TeacherCard key={teacher._id} teacher={teacher} />
+        <div key={teacher._id}>
+          <TeacherCard
+            teacher={teacher}
+            onClick={() => handleTeacherClick(teacher._id)}
+            isSelected={selectedTeacherId === teacher._id}
+          />
+        </div>
       ))}
+
+      {selectedTeacherId &&
+        (subjects[selectedTeacherId] &&
+        subjects[selectedTeacherId].length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Curso</th>
+                <th>Grupo</th>
+                <th>Horas</th>
+                <th>Espacio</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {subjects[selectedTeacherId].map((subject: Subject) => (
+                <tr key={subject._id}>
+                  <td>{subject.name}</td>
+                  <td>{subject.type}</td>
+                  <td>{subject.grade}</td>
+                  <td>{subject.group}</td>
+                  <td>{subject.hours}</td>
+                  <td>{subject.espacio}</td>
+                  <td>
+                    <button>Ver</button>
+                    <button>Editar</button>
+                    <button>Eliminar</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No hay asignaturas asignadas a este profesor</p>
+        ))}
     </div>
   );
 }
