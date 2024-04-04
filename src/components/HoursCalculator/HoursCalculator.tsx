ace750ab401f85e3133fde83f7850177cfeacba5
ayozemt <ayozemt@gmail.com>
2024-04-04 14:27:01 +0200

    Apply style to HoursCalculator component

--- src/components/HoursCalculator/HoursCalculator.tsx
+++ src/components/HoursCalculator/HoursCalculator.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
 function HoursCalculator({ teachingHours }: { teachingHours: number }) {
   const [totalHours, setTotalHours] = useState(0);
@@ -20,18 +21,47 @@ function HoursCalculator({ teachingHours }: { teachingHours: number }) {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={() => handleTabChange("weekly")}>Semanal</button>
-        <button onClick={() => handleTabChange("annual")}>Anual</button>
+    <div className="d-flex flex-column align-items-center">
+      <div
+        className="btn-group border border-secondary p-1 mt-4 bg-secondary"
+        role="group"
+      >
+        <Button
+          variant={
+            activeTab === "weekly"
+              ? "light rounded px-3"
+              : "secondary rounded px-3"
+          }
+          onClick={() => handleTabChange("weekly")}
+        >
+          Semanal
+        </Button>
+        <Button
+          variant={
+            activeTab === "annual"
+              ? "light rounded px-4 mx-1"
+              : "secondary rounded px-4 mx-1"
+          }
+          onClick={() => handleTabChange("annual")}
+        >
+          Anual
+        </Button>
       </div>
-      <div>
-        <div>Horas Totales: {totalHours}</div>
-        <div>
-          Horas Lectivas:{" "}
-          {activeTab === "weekly" ? teachingHours : teachingHours * 37}
+      <div className="d-flex justify-content-around w-100">
+        <div className="my-5 py-2 px-5 border rounded">
+          <p className="m-1">Horas Totales:</p>
+          <h1 className="m-1">{totalHours}</h1>
         </div>
-        <div>Horas Complementarias: {extraHours}</div>
+        <div className="my-5 py-2 px-5 border rounded">
+          <p className="m-1">Horas Lectivas:</p>
+          <h1 className="m-1">
+            {activeTab === "weekly" ? teachingHours : teachingHours * 37}
+          </h1>
+        </div>
+        <div className="my-5 py-2 px-5 border rounded">
+          <p className="m-1">Horas Complementarias:</p>{" "}
+          <h1 className="m-1">{extraHours}</h1>
+        </div>
       </div>
     </div>
   );
